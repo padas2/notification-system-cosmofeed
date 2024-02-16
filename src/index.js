1ee435f4d0c60a947fd6f28f8a1c6998414bcac4
Supreeth Padavala <supreeth.padavala@infra.market>
2024-02-16 21:48:22 +0530

    Adding fallback safety code

--- src/index.js
+++ src/index.js
@@ -5,11 +5,17 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.server = void 0;
 const http_1 = __importDefault(require("http"));
+const router = require('find-my-way')();
+router.on('GET', '/', (req, res, params) => {
+    console.log("Nothing to see here...");
+    res.end('{"message":"Nothing to see here... try something else"}');
+});
+router.on('POST', '/notifications/send', (req, res, params) => {
+    console.log(req.body);
+    res.end('{"message":"Notification will be sent"}');
+});
 exports.server = http_1.default.createServer((req, res) => {
-    res.writeHead(200, { "Content-Type": "application/json" });
-    res.end(JSON.stringify({
-        data: "It Works!",
-    }));
+    router.lookup(req, res);
 });
 exports.server.listen(3000, () => {
     console.log("Server running on http://localhost:3000/");
--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,19 @@
 import http from "http";
+const router = require('find-my-way')()
+
+router.on('GET', '/', (req: any, res: any, params: any) => {
+  console.log("Nothing to see here...")
+  res.end('{"message":"Nothing to see here... try something else"}')
+})
+
+router.on('POST', '/notifications/send', (req: any, res: any, params: any) => {
+  console.log(req.body)
+  res.end('{"message":"Notification will be sent"}')
+})
 
 export const server = http.createServer((req, res) => {
-  res.writeHead(200, { "Content-Type": "application/json" });
-  res.end(
-    JSON.stringify({
-      data: "It Works!",
-    })
-  );
-});
+  router.lookup(req, res)
+})
 
 server.listen(3000, () => {
   console.log("Server running on http://localhost:3000/");
